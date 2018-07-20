713b3ee01ee23b460ada160488714487d93ccd44
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-07-20 15:25:42 +0900

    test admin-only functionality

--- test/test_with_sudo.py
+++ test/test_with_sudo.py
@@ -1,18 +1,21 @@
-"""Tests for all_info module."""
+"""Tests for functionality that requires administrative access."""
 
 import collections.abc
+import getpass
 import unittest
 
 from system_query.all_info import query_all
 
 
+@unittest.skipUnless(getpass.getuser() == 'root', 'skipping tests that require sudo')
 class Tests(unittest.TestCase):
 
-    def test_query_all(self):
-        info = query_all(sudo=False)
+    def test_query_all_sudo(self):
+        info = query_all(sudo=True)
         self.assertIsInstance(info, collections.abc.Mapping)
         self.assertGreater(len(info), 1)
         self.assertIn('host', info)
         self.assertIn('os', info)
         self.assertIn('cpu', info)
         self.assertIn('ram', info)
+        self.assertGreater(len(info.get('ram', {}).get('banks', [])), 1)
