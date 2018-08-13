3c75c83c32f8c52e4c649d6e92d1dc8cda94e421
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2018-08-12 19:15:47 -0500

    badly written test...

--- test/test_with_sudo.py
+++ test/test_with_sudo.py
@@ -20,7 +20,7 @@ class Tests(unittest.TestCase):
         self.assertIn('cpu', info)
         self.assertIn('ram', info)
         min_length = 1 if platform.system() == 'Linux' else 0
-        ram_banks = len(info.get('ram', {}).get('banks', []))
-        self.assertGreaterEqual(ram_banks, min_length)
+        ram_banks = info.get('ram', {}).get('banks', [])
+        self.assertGreaterEqual(len(ram_banks), min_length)
         for ram_bank in ram_banks:
             self.assertIsInstance(ram_bank, dict)
