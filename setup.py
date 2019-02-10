3129cf3e297767c8a9ce15a91de04a56d7d4741b
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2019-02-10 19:11:31 +0900

    officially support Python 3.7

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ class Package(setup_boilerplate.Package):
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3 :: Only',
         'Topic :: Education',
         'Topic :: Scientific/Engineering',
