7bf1d6cae3566fbee0740c09257381abf7be74db
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2023-09-23 19:16:11 +0900

    build: use logging boilerplate

--- system_query/__main__.py
+++ system_query/__main__.py
@@ -1,7 +1,19 @@
 """Entry point of the command-line interface of system_query package."""
 
+import logging
+
+import boilerplates.logging
+
 from .main import main
 
 
+class Logging(boilerplates.logging.Logging):
+    """Logging configuration."""
+
+    packages = ['system_query']
+    level_package = logging.WARNING
+
+
 if __name__ == '__main__':
+    Logging.configure()
     main()
