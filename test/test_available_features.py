206ca66a99ad7b321302baf737fae7d61b6d4c23
Mateusz Bysiek <mateusz.bysiek@gmail.com>
2023-09-27 20:46:33 +0900

    feat: include swap query in various contexts it was missing from

--- test/test_available_features.py
+++ test/test_available_features.py
@@ -2,8 +2,8 @@
 
 import unittest
 
-from system_query.available_features import CPU, GPU, HDD
-from system_query import query_cpu, query_gpus, query_hdds
+from system_query.available_features import CPU, GPU, HDD, SWAP
+from system_query import query_cpu, query_gpus, query_hdds, query_swap
 
 
 class Tests(unittest.TestCase):
@@ -19,3 +19,7 @@ class Tests(unittest.TestCase):
     @unittest.skipUnless(HDD, 'querying HDDs is not supported')
     def test_hdds(self):
         self.assertGreaterEqual(len(query_hdds()), 1)
+
+    @unittest.skipUnless(SWAP, 'querying swap is not supported')
+    def test_swap(self):
+        self.assertGreaterEqual(len(query_swap()), 1)
